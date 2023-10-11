c740166738a29b7826c6a33d31780ced6f86ce14
gupta-parag <parag.gupta@protonmail.com>
2023-10-10 23:54:56 -0400

    Added cube

--- ch1/app.js
+++ ch1/app.js
@@ -24,6 +24,8 @@ const sizes = {
 
 // Creating a camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 1;
 scene.add(camera);
 
 //Renderer
